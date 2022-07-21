8b148d26c6f119b0b1e203cb82766dfcc910ae3c
Артём Нечай <lenikol95@mail.ru>
2022-07-21 12:04:53 +0300

    add places model

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -1,1 +1,7 @@
 """Admin module of Palces app."""
+
+from django.contrib import admin
+
+from where_to_go.places.models import Place
+
+admin.site.register(Place)
--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -1,1 +1,29 @@
 """Models for places app."""
+
+from django.db import models
+
+TITLE_MAX_LENGTH = 200
+
+
+class Place(models.Model):
+    """Class for objects of interesting places to visit."""
+
+    title = models.CharField(unique=True, max_length=TITLE_MAX_LENGTH, verbose_name='Название')
+    short_description = models.TextField(verbose_name='Краткое описание', blank=True)
+    full_description = models.TextField(verbose_name='Полное описание', blank=True)
+    latitude = models.FloatField(verbose_name='Широта')
+    longitude = models.FloatField(verbose_name='Долгота')
+
+    class Meta(object):
+        """Meta information of model."""
+
+        verbose_name = 'Место интереса'
+        verbose_name_plural = 'Места интереса'
+
+    def __str__(self) -> str:
+        """Present object as a string.
+
+        Returns:
+            Title of place.
+        """
+        return self.title
